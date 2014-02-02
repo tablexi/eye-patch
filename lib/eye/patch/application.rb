d6096e6f96d3cfba3aa95649670f81eae6fa8866
Andrew Horner <andrew@tablexi.com>
2014-02-02 14:10:20 -0600

    Copy root-level application settings to processes

--- lib/eye/patch/application.rb
+++ lib/eye/patch/application.rb
@@ -2,61 +2,68 @@ class Eye::Patch::Application < Hash
 
   def initialize(settings)
     super()
-    self[settings[:name].to_s] = parse(settings)
+    @settings = settings
+    self[settings[:name].to_s] = parse
   end
 
   private
 
-  def parse(settings)
+  def parse
     @config = {}
 
-    parse_application(settings)
-    parse_notifications(settings)
-    parse_triggers(settings)
-    parse_processes(settings)
+    parse_application
+    parse_notifications
+    parse_triggers
+    parse_processes
 
     @config
   end
 
-  def parse_application(settings)
-    @config.merge!(settings[:application] || {})
+  def root_settings
+    @settings[:application] || {}
   end
 
-  def parse_notifications(settings)
+  def parse_application
+    @config.merge!(root_settings)
+  end
+
+  def parse_notifications
     @config[:notify] = {}
 
-    Array(settings[:notifications]).each do |monitor|
+    Array(@settings[:notifications]).each do |monitor|
       @config[:notify][monitor[:name].to_s] = monitor[:level].to_sym
     end
   end
 
-  def parse_triggers(settings)
+  def parse_triggers
     @config[:triggers] = {}
 
-    Array(settings[:triggers]).each do |item|
+    Array(@settings[:triggers]).each do |item|
       trigger_data = Eye::Trigger.name_and_class(item[:name].to_sym)
       @config[:triggers][trigger_data[:name]] = item[:config].merge(type: trigger_data[:type])
     end
   end
 
-  def parse_processes(settings)
+  def parse_processes
     @config[:groups] = {}
 
-    Array(settings[:processes]).group_by{ |item| item[:group] }.each do |group_name, items|
+    Array(@settings[:processes]).group_by{ |item| item[:group] }.each do |group_name, items|
       name = group_name || "__default__"
-
-      @config[:groups][name.to_s] = { application: settings[:name].to_s }
-      parse_group(name.to_s, items)
+      parse_group(name, items)
     end
   end
 
   def parse_group(name, processes)
-    @config[:groups][name][:processes] = parse_process_list(name, processes)
+    @config[:groups][name] = root_settings.merge(
+      application: @settings[:name].to_s,
+      processes: parse_process_list(name, processes))
   end
 
   def parse_process_list(group_name, processes)
     processes.each_with_object({}) do |process, process_map|
-      process_map[process[:name].to_s] = process[:config].merge(group: group_name)
+      process_map[process[:name].to_s] = root_settings.merge(process[:config]).merge(
+        name: process[:name].to_s,
+        group: group_name )
     end
   end
 end
