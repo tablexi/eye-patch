26aa33d4f42e210792d4a264707e62ea34ae278e
Andrew Horner <andrew@tablexi.com>
2014-02-04 11:27:42 -0600

    Reload eye configuration before restart on deploy

--- lib/eye/patch/capistrano.rb
+++ lib/eye/patch/capistrano.rb
@@ -7,14 +7,14 @@ Capistrano::Configuration.instance.load do
 
   if fetch(:eye_default_hooks)
     after  "deploy:stop",    "eye:stop"
-    after  "deploy:start",   "eye:start"
+    after  "deploy:start",   "eye:load"
     before "deploy:restart", "eye:restart"
   end
 
   namespace :eye do
 
     desc "Start eye with the desired configuration file"
-    task :start, roles: -> { fetch(:eye_roles) } do
+    task :load, roles: -> { fetch(:eye_roles) } do
       run "cd #{current_path} && #{fetch(:eye_bin)} l #{fetch(:eye_config)}"
     end
 
@@ -28,4 +28,6 @@ Capistrano::Configuration.instance.load do
       run "cd #{current_path} && #{fetch(:eye_bin)} r all"
     end
   end
+
+  before "eye:restart", "eye:load"
 end
