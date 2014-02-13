3d50897ade924c2423657bb88beb542a987c519f
Andrew Horner <andrew@tablexi.com>
2014-02-13 16:18:29 -0600

    Allow processes to be spawned without closing fds

--- lib/eye/patch/overrides.rb
+++ lib/eye/patch/overrides.rb
@@ -7,6 +7,27 @@ Eye::Cli.class_eval do
   end
 end
 
+Eye::System.class_eval do
+  private
+
+  def spawn_options(config = {})
+    o = {pgroup: true, chdir: config[:working_dir] || '/'}
+    o.update(out: [config[:stdout], 'a']) if config[:stdout]
+    o.update(err: [config[:stderr], 'a']) if config[:stderr]
+    o.update(in: config[:stdin]) if config[:stdin]
+    o.update(close_others: !config[:preserve_fds])
+
+    if Eye::Local.root?
+      o.update(uid: Etc.getpwnam(config[:uid]).uid) if config[:uid]
+      o.update(gid: Etc.getpwnam(config[:gid]).gid) if config[:gid]
+    end
+
+    o.update(umask: config[:umask]) if config[:umask]
+
+    o
+  end
+end
+
 Eye::Controller.class_eval do
   private
 
