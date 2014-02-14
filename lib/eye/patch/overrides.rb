ee5e9b7722b334b22b162f95a4ec65c84a54d67c
Andrew Horner <andrew@tablexi.com>
2014-02-13 20:31:43 -0600

    Add process before_spawn hook support
    Specific applications may need to invoke specific behaviors prior to
    spawning a new process, such as writing temporary information to a file 
    descriptor.

--- lib/eye/patch/overrides.rb
+++ lib/eye/patch/overrides.rb
@@ -8,27 +8,50 @@ Eye::Cli.class_eval do
 end
 
 Eye::System.class_eval do
-  private
+  class << self
+    alias_method :daemonize_without_hook, :daemonize
+    alias_method :exec_without_hook, :exec
 
-  def spawn_options(config = {})
-    o = {pgroup: true, chdir: config[:working_dir] || '/'}
-    o.update(out: [config[:stdout], 'a']) if config[:stdout]
-    o.update(err: [config[:stderr], 'a']) if config[:stderr]
-    o.update(in: config[:stdin]) if config[:stdin]
-    o.update(close_others: !config[:preserve_fds])
-
-    if Eye::Local.root?
-      o.update(uid: Etc.getpwnam(config[:uid]).uid) if config[:uid]
-      o.update(gid: Etc.getpwnam(config[:gid]).gid) if config[:gid]
+    def daemonize(*args)
+      Eye::Control.invoke_spawn_callback
+      daemonize_without_hook(*args)
     end
 
-    o.update(umask: config[:umask]) if config[:umask]
+    def exec(*args)
+      Eye::Control.invoke_spawn_callback
+      exec_without_hook(*args)
+    end
 
-    o
+    private
+
+    def spawn_options(config = {})
+      o = {pgroup: true, chdir: config[:working_dir] || '/'}
+      o.update(out: [config[:stdout], 'a']) if config[:stdout]
+      o.update(err: [config[:stderr], 'a']) if config[:stderr]
+      o.update(in: config[:stdin]) if config[:stdin]
+      o.update(close_others: !config[:preserve_fds])
+
+      if Eye::Local.root?
+        o.update(uid: Etc.getpwnam(config[:uid]).uid) if config[:uid]
+        o.update(gid: Etc.getpwnam(config[:gid]).gid) if config[:gid]
+      end
+
+      o.update(umask: config[:umask]) if config[:umask]
+
+      o
+    end
   end
 end
 
 Eye::Controller.class_eval do
+
+  def invoke_spawn_callback
+    if respond_to?(:before_spawn)
+      debug "Invoking before_spawn hook"
+      before_spawn
+    end
+  end
+
   private
 
   def parse_config(filename)
