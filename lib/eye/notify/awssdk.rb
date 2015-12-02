a4028b9a779423412ec17a906fff112e6ac9958d
Robin Dunlop <robin@dunlopweb.com>
2015-12-02 11:36:56 -0600

    un-compactify module declaration. Are you happy now Hound?

--- lib/eye/notify/awssdk.rb
+++ lib/eye/notify/awssdk.rb
@@ -1,39 +1,41 @@
 require "aws-sdk-core"
 require "aws-sdk-core/ses"
 
-module Eye::Notify
-  class AWSSDK < Eye::Notify
-    param :region, String, true
-    param :access_key_id, String, true
-    param :secret_access_key, String, true
-    param :from, String, true
+module Eye
+  module Notify
+    class AWSSDK < Eye::Notify
+      param :region, String, true
+      param :access_key_id, String, true
+      param :secret_access_key, String, true
+      param :from, String, true
 
-    def execute
-      client = Aws::SES::Client.new(
-        region: region,
-        credentials: Aws::Credentials.new(access_key_id, secret_access_key))
-      client.send_email(message)
-    end
+      def execute
+        client = Aws::SES::Client.new(
+          region: region,
+          credentials: Aws::Credentials.new(access_key_id, secret_access_key))
+        client.send_email(message)
+      end
 
-    def message
-      { source: from,
-        destination: {
-          to_addresses: [contact]
-        },
-        message: {
-          subject: {
-            data: message_subject
+      def message
+        { source: from,
+          destination: {
+            to_addresses: [contact]
           },
-          body: {
-            text: {
-              data: message_body
+          message: {
+            subject: {
+              data: message_subject
             },
-            html: {
-              data: message_body
+            body: {
+              text: {
+                data: message_body
+              },
+              html: {
+                data: message_body
+              }
             }
           }
         }
-      }
+      end
     end
   end
 end
