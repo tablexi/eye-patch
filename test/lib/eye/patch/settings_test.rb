1b67867d10e7506e3b9ca04812b7121a89bf627c
Andrew Horner <andrew@tablexi.com>
2016-02-04 16:38:56 -0600

    Fix line length issue in Settings test

--- test/lib/eye/patch/settings_test.rb
+++ test/lib/eye/patch/settings_test.rb
@@ -16,7 +16,10 @@ module Eye
         file.write("working_dir: <%= __FILE__ %>/..")
         file.close
 
-        assert_equal File.join(file.path, ".."), Settings.new(file.path)[:working_dir]
+        assert_equal(
+          File.join(file.path, ".."),
+          Settings.new(file.path)[:working_dir],
+        )
       end
     end
   end
