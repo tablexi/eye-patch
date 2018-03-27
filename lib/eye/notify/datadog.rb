853775e38bff7bbab2371e11ef33abba69c6cc5e
Lloyd Philbrook <lloyd@tablexi.com>
2018-03-27 11:16:35 -0700

    ADD datadog notifcation

---
+++ lib/eye/notify/datadog.rb
@@ -0,0 +1,40 @@
+require "dogapi"
+
+module Eye
+  class Notify
+    class DataDog < Eye::Notify
+      param :aggregation_key, String
+      param :alert_type, String
+      param :api_key, String, true
+      param :source_type, String
+      param :tags, Array
+
+      def execute
+        options = {
+          alert_type: "error",
+          aggregation_key: msg_host + msg_full_name,
+          source_type: "None",
+          tags: [ "eye" ],
+        }
+
+        options[:alert_type] = alert_type if alert_type
+        options[:aggregation_key] = aggregation_key if aggregation_key
+        options[:source_type] = source_type if source_type
+        options[:tags] = tags if tags
+
+        dog = Dogapi::Client.new(api_key)
+
+        dog.emit_event(Dogapi::Event.new(
+            message_body,
+            aggregation_key: options[:aggregation_key],
+            alert_type: options[:alert_type],
+            msg_title: message_subject,
+            host: msg_host,
+            source_type: options[:source_type]
+            tags: options[:tags],
+          )
+        )
+      end
+    end
+  end
+end
